5eff7c993cfc3206f7d3a7a7e735b85953ed616b
Daniel Malinowski <malinowski@wapp.gmbh>
2023-07-07 11:12:14 +0200

    add dependencies to package description

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WappUIComponents",
-            dependencies: []
+            dependencies: ["VisualEffectView", "SnapKit", "Atributika", ],
         ),
     ]
 )
